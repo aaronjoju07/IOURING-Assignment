a9e36a1406052722c3134950153324b5e07d222e
aaronjoju07 <aaronjoju07@gmail.com>
2025-02-13 09:33:14 +0530

    add protected routes for home page access

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
 import Home from './Page/Home'
 import Login from './Page/Login'
+import ProtectedRoutes from './utils/ProtectedRoutes'
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route element={<ProtectedRoutes />} >
+        <Route path="/" element={<Home />} />
+        </Route>
       </Routes>
     </BrowserRouter>
 
